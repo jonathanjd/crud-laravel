8bf058189faf7d9672e40bbb13d869e32b36e78f
Jonathan Duran <headjd@gmail.com>
2018-01-26 09:34:19 -0400

    Instalación de paquetes(Collective+LaracastsFlash)

--- config/app.php
+++ config/app.php
@@ -168,7 +168,8 @@ return [
          */
 
         //
-
+        Collective\Html\HtmlServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
